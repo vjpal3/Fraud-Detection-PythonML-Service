7823cdf1cadf21fc90c03d4abb88fd52853cf395
vjpal3 <vrishalipal@yahoo.com>
2020-05-25 14:49:41 -0400

    create model with logistic regression

--- fraud_detection.py
+++ fraud_detection.py
@@ -4,10 +4,56 @@ import pandas as pd
 def train_model():
     
     # Import the dataset
-    dataset = pd.read_csv('data/PS_Sample_log.csv')
+    dataset = pd.read_csv('data/PS_20174392719_1491204439457_log.csv')
+    """dataset = pd.read_csv('data/PS_Sample_log.csv')"""
     
     X = dataset.iloc[:, [2, 4, 5, 7, 8]].values
     y = dataset.iloc[:, 9].values
+    
+    y_target = dataset['isFraud']
+    
+    # Splitting the dataset into the Training set and Test set
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0, stratify=y_target)
+    
+    # Training the Logistic Regression model on the Training set
+    from sklearn.linear_model import LogisticRegression
+    classifier = LogisticRegression(random_state = 0)
+    classifier.fit(X_train, y_train)
+    
+    # Predicting the Test set results
+    y_pred = classifier.predict(X_test)
+    
+    # Making the Confusion Matrix
+    from sklearn.metrics import confusion_matrix
+    cm = confusion_matrix(y_test, y_pred)
+    """print(cm)"""
+    
+    tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+    
+    from sklearn import metrics
+    accuracy = round(metrics.accuracy_score(y_test, y_pred),4)*100
+    precision = round(metrics.precision_score(y_test, y_pred),4)*100
+    recall = round(metrics.recall_score(y_test, y_pred),4)*100
+    
+    print("Accuracy:     ", accuracy)
+    print("Precision:    ", precision)
+    print("Recall:       ", recall)
+    
+    result = [
+        {
+            'Test data Size': y_test.size,
+            'Non-Fradulent predicted True': tn.item(),
+            'Non-Fradulent predicted false': fn.item(),
+            'Fradulent predicted True': tp.item(),
+            'Fradulent predicted false': fp.item()
+        },
+        
+        {
+            'Accuracy': accuracy,
+            'Precision': precision,
+            'Recall': recall
+        }
+    ]
 
-    result = [{"results": "model performance statistics here"}]
     return result
